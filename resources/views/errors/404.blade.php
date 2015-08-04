cae7826ac96e837c4269f4a60a35e397fc55878a
Anna Fleming <malyshkoanya@gmail.com>
2015-08-04 02:31:36 +0000

    404 page added

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="error404-block">
-        404
+        <h1>That’s not going<br/>to work.</h1>
+        <div class="error-body">
+            <div class="message">
+                <p>This webpage doesn’t exist.</p>
+                <p><a href="/">Fly home</a></p>
+            </div>
+            <img src="/images/birds.svg" alt="Birds"/>
+        </div>
+
     </div>
 @endsection('content')
