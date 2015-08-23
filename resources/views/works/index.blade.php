5060c9eead4b98d84112e5fee1d747501e8ec22c
Anna Fleming <malyshkoanya@gmail.com>
2015-08-23 21:38:42 +0000

    workes fixed]

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -10,19 +10,24 @@
         <div class="works-list">
             @foreach($works as $work)
                 <article>
+                    <div class="work-wrapper">
+                        <div class="work-content">
+                            @if ($work->image)
+                                <img src="{{ $work->getFilePath() }}" />
+                            @endif
+                            <div class="work-info" style="{{($work->image) ? 'display:none': ''}}">
+                                {{ $work->description }}
+                                <br/><br/>/role/<br/><br/>
+                                {{ $work->my_role }}
+                                <br/><br/>
+                                Go to {!! link_to($work->link, $work->name) !!}
+                            </div>
+                        </div>
+                    </div>
                     <div class="work-head">
                         {{ $work->name }}
                     </div>
-                    @if ($work->image)
-                        <img src="{{ $work->getFilePath() }}" />
-                    @endif
-                    <div class="work-info" style="{{($work->image) ? 'display:none': ''}}">
-                        {{ $work->description }}
-                        <br/><br/>/role/<br/><br/>
-                        {{ $work->my_role }}
-                        <br/><br/>
-                        Go to {!! link_to($work->link, $work->name) !!}
-                    </div>
+
                 </article>
             @endforeach
         </div>
