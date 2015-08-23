dccd6e99c6e53a6db2caa01fe0a751dad487e424
Anna Fleming <malyshkoanya@gmail.com>
2015-08-23 21:56:06 +0000

    fix

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,6 +3,6 @@
 </a>
 <nav>
     <a href="/works" class="{{ Request::is('works') ? 'active' : '' }}">Works</a>
-    <a href="/cv" class="{{ Request::is('cv') ? 'active' : '' }}"">CV</a>
+    <a href="/cv" class="{{ Request::is('cv') ? 'active' : '' }}">CV</a>
     <a href="/contact" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a>
 </nav>
