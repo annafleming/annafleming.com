731c57f1cc81c676da60c6be74f15f14ffcb68e9
Anna Fleming <malyshkoanya@gmail.com>
2015-08-24 05:03:05 +0000

    fixes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,15 +1,16 @@
-@extends('app')
+@extends('appblue')
 
 @section('content')
     <div class="error404-block">
-        <h1>That’s not going<br/>to work.</h1>
-        <div class="error-body">
-            <div class="message">
-                <p>This webpage doesn’t exist.</p>
-                <p><a href="/">Fly home</a></p>
+        <div class="error404-inner">
+            <h1>That’s not going<br/>to work.</h1>
+            <div class="error-body">
+                <div class="message">
+                    <p>This webpage doesn’t exist.</p>
+                    <p><a href="/">Fly home</a></p>
+                </div>
+                <img src="/images/birds.svg" alt="Birds"/>
             </div>
-            <img src="/images/birds.svg" alt="Birds"/>
         </div>
-
     </div>
 @endsection('content')
